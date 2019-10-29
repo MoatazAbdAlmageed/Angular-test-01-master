e909295aa395bece4bd82b282520ca7271d6a5b5
Moataz Mohammady <moataz.mohammady@gmail.com>
2019-10-29 07:47:45 +0200

    Add `completed` property to  `TodoModel`

--- src/resources/todoResource.ts
+++ src/resources/todoResource.ts
@@ -15,22 +15,27 @@ export class TodoResource {
         {
             id:1,
             todo:'IBM Interview',
+            completed:true,
             date:new Date()
         },{
             id:2,
             todo:'Task 2',
+            completed:false,
             date:new Date()
         },{
             id:3,
             todo:'Task 3',
+            completed:false,
             date:new Date()
         },{
             id:4,
             todo:'Task 4',
+            completed:false,
             date:new Date()
         },{
             id:5,
             todo:'Task 5',
+            completed:false,
             date:new Date()
         },
     ];
